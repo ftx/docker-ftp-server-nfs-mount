4e1a2f9a602014089a9e43846dfe61895449343e
Florian Mauduit <flotix@linux.com>
2015-11-30 14:21:24 +0100

    First

---
+++ README.md
@@ -0,0 +1,22 @@
+## FTP Server with NFS Mount 
+
+
+##### FTP
+|ENV Variable  |Required |Default   |Example   |
+|---|---|---|---|
+|FTP   |NO   |**NO**   |YES   |
+|FTP_LOGIN   |NO   |flotix   |ftx   |
+|FTP_PASSWD   |NO   |**CHANGEME**   |lsdfgzeihti$!976756   |
+
+##### NFS
+|ENV Variable  |Required |Default   |Example   |
+|---|---|---|---|
+|NFS_SERVER   |NO   |**NO**   |YES   |
+|NFS_REMOTE   |YES   |/nfs   |/var/www   |
+|NFS_LOCAL   |YES   |/nfs   |/var/www   |
+
+
+Don't hesitate to make a pull request!! :kissing_heart:
+
+Contact : flotix@linux.com
+Website : www.flotix.jp
---
+++ bin/run.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set -e
+[ "$DEBUG" == "1" ] && set -x && set +e
+
+
+if [ "${NFS_SERVER}" == "*" -o -z "${NFS_SERVER}" ]; then
+   NFS_SERVER=${NFS_SERVER}
+fi
+
+
+if [ "${NFS_REMOTE}" == "*" -o -z "${NFS_REMOTE}" ]; then
+   NFS_REMOTE=${NFS_REMOTE}
+fi
+
+
+### MOUNT NFS
+
+mount -t nfs ${NFS_SERVER}:${NFS_REMOTE} /nfs
+
+
+### FTP
+if [ "${FTP}" == "**NO**" -o -z "${FTP}" ]; then
+   FTP=${FTP}
+fi
+
+
+if [ "${FTP}" == "YES" ]; then
+
+if [ "${FTP_LOGIN}" == "flotix" -o -z "${FTP_LOGIN}" ]; then
+   FTP_LOGIN=${FTP_LOGIN}
+fi
+
+if [ "${FTP_PASSWD}" == "**ChangeMe**" -o -z "${FTP_PASSWD}" ]; then
+   FTP_PASSWD=${FTP_PASSWD}
+fi
+
+
+# User
+adduser --quiet --disabled-password -shell /bin/false --home /nfs --gecos "FTP" ${FTP_LOGIN}
+echo "${FTP_LOGIN}:${FTP_PASSWD}" | chpasswd
+
+
+
+
+fi
+
+/usr/bin/supervisord
